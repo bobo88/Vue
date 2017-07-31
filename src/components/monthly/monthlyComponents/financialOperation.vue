282b53418e21acee525e7f775c859c51855e841d
BOBO <yuanboi88@163.com>
2017-07-31 14:36:17 +0800

    测试 添加财务运营KPI

--- src/components/monthly/monthlyComponents/financialOperation.vue
+++ src/components/monthly/monthlyComponents/financialOperation.vue
@@ -16,7 +16,63 @@ export default {
     return {
       msg: ''
     }
-  }
+  },
+  mounted(){
+		this.$nextTick(function() {
+            var todayTop3Chart = echarts.init(document.getElementById('todayTop3'));
+            var todayTop3Option = {
+                title: {
+                    text: '今日top3',
+                    top: 10,
+                    left: 10,
+                    textStyle: {
+                        color: '#344559',
+                        fontSize: 14,
+                        fontWeight: 'normal'
+                    }
+                },
+                tooltip : { // 提示框组件
+                    trigger: 'axis'
+                },
+                grid: { // 直角坐标系网格组件
+                    left: '15%',
+                    height: '60%',
+                    width: '80%'
+                },
+                xAxis : [
+                    {  
+                        splitLine:{
+                            lineStyle: {
+                                color: '#eee'
+                            }
+                        },
+                        type : 'value'
+                    }
+                ],
+                yAxis : [
+                    {
+                        splitLine:{show: false}, //去除网格线
+                        type : 'category',
+                        data : ['大连日航','北京唐拉','东莞唐拉']
+                    }
+                ],
+                series : [
+                    {
+                        type:'bar',
+                        data:[18203, 23489, 29034],
+                        itemStyle:{
+                            normal: {
+                                color: '#87ADE3'
+                            }
+                        },
+                        barCategoryGap: '70%' // 类目间柱形距离，默认为类目间距的20%，可设固定值。值越大，bar越细
+                    }
+                ]
+            };
+            // 使用刚指定的配置项和数据显示图表。
+            todayTop3Chart.setOption(todayTop3Option);
+        })
+	}
 }
 </script>
 
